c6f0cbbdb38e8380207d222be6fa5e5aad683ddf
rayct <rayct@icloud.com>
2021-07-25 01:18:24 +0100

    ES6 Update - 8-31

--- fcc-scratchpad.js
+++ fcc-scratchpad.js
@@ -1,3 +1,104 @@
+// Use the Rest Parameter with Function Parameters
+
+
+
+
+
+// Set Default Parameters for Your Functions
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+
+// Write Arrow Functions with Parameters
+const myConcat = (arr1, arr2) => {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+// Use Arrow Functions to Write Concise Anonymous Functions
+const magic = () => {
+  return new Date();
+};
+console.log(magic);
+
+// Prevent Object Mutation
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // Only change code below this line
+
+  Object.freeze(MATH_CONSTANTS);
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+
+// Mutate an Array Declared with const
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+
+s [0] = 2;
+s [1] = 5;
+s [2] = 7;
+  
+
+  // Only change code above this line
+}
+editInPlace();
+console.log(s);
+ 
+// Declare a Read-Only Variable with the const Keyword
+function printManyTimes(str) {
+
+  // Only change code below this line
+
+  const SENTENCE = str + " is cool!";
+  // const SENTENCE = str + "is amazing!";
+
+  for (let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // Only change code above this line
+
+}
+printManyTimes("freeCodeCamp");
+
+
+//Compare Scopes of the var and let Keywords.
+function checkScope() {
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+checkScope();
+
+// ES6 Explore Differences Between the var and let Keywords
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
+console.log(catTalk(data));
 // Use Recursion to Create a Range of Numbers
 function rangeOfNumbers(startNum, endNum) {
   if (startNum === endNum) {
