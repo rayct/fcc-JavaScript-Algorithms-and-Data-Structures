3fb06ab7d5bb5a79c87448760d2c1c5ff7b0d465
rayct <rayct@icloud.com>
2021-07-26 01:50:53 +0100

    Updated Course - 10/31

--- fcc-scratchpad.js
+++ fcc-scratchpad.js
@@ -1,8 +1,25 @@
+// Use Destructuring Assignment to Extract Values from Objects
+
+
+
+
+
+
+
+// Use the Spread Operator to Evaluate Arrays In-Place
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
+
 // Use the Rest Parameter with Function Parameters
+const sum = (...args) => { 
+    return args.reduce((a, b) => a + b, 0);
+  };
 
-
-
-
+console.log(sum(1, 2, 3, 4));
 
 // Set Default Parameters for Your Functions
 // Only change code below this line
