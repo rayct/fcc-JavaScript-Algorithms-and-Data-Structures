1c4e498132f1ebf71ff49e942cf1d0f19d2b2fc9
rayct <rayct@icloud.com>
2021-08-01 18:00:54 +0100

    JS Course Update - ES6 - 12/33

--- fcc-scratchpad.js
+++ fcc-scratchpad.js
@@ -1,3 +1,24 @@
+
+
+
+
+
+
+// Use Destructuring Assignment to Assign Variables from Objects
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+  const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+  // const highToday = HIGH_TEMPERATURES.today;
+  // const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+  console.log(HIGH_TEMPERATURES);
+// Only change code above this line
+
+
 // Use Destructuring Assignment to Extract Values from Objects
 const HIGH_TEMPERATURES = {
   yesterday: 75,
