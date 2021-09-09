7811bfc61e20182dcbb19905cb5e91087124a86e
rayct <rayct@icloud.com>
2021-09-09 16:13:48 +0100

    Course Update = Q9/16

--- fcc-JavaScript-CourseWork.js
+++ fcc-JavaScript-CourseWork.js
@@ -1,4 +1,25 @@
 // ============================== BASIC ALGORITHM SCRIPTING ================================ //
+// Basic Algorithm Scripting = Q9/16 - Finders Keepers
+// NOTES: A function that looks through an array arr and returns the first element in it that passes a 'truth test'.
+// This means that given an element x, the 'truth test' is passed if func(x) is true.
+// If no element passes the test, return undefined.
+function findElement(arr, func) {
+  let num = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (func(arr[i])) {
+      return arr[i];
+    
+    }
+  }
+  return undefined;
+  
+}
+
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+// console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }));
+// console.log(findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }));/
+
 // Basic Algorithm Scripting = Q8/16 - Truncate a String
 // NOTES:
 function truncateString(str, num) {
