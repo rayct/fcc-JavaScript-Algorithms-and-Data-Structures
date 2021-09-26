91a9b9e77ab1b8e0fbf89942c527f531cb426795
rayct <rayct@icloud.com>
2021-09-26 17:16:34 +0100

    Course Update Q = 3/26

--- fcc-JavaScript-CourseWork.js
+++ fcc-JavaScript-CourseWork.js
@@ -45,11 +45,57 @@
 
 // Functional Programming = Q4/26 - 
 
-// Functional Programming = Q3/26 - 
+// Functional Programming = Q3/26 - Understand the Hazards of Using Imperative Code
+// TASK: Change Window.prototype.tabClose so that it removes the correct tab.
+// tabs is an array of titles of each site open within the window
+var Window = function(tabs) {
+  this.tabs = tabs; // We keep a record of the array inside the object
+};
+
+// When you join two windows into one window
+Window.prototype.join = function (otherWindow) {
+  this.tabs = this.tabs.concat(otherWindow.tabs);
+  return this;
+};
+
+// When you open a new tab at the end
+Window.prototype.tabOpen = function (tab) {
+  this.tabs.push('new tab'); // Let's open a new tab for now
+  return this;
+};
+
+// When you close a tab
+Window.prototype.tabClose = function (index) {
+
+  // Only change code below this line
+
+  var tabsBeforeIndex = this.tabs.splice(0, index); // Get the tabs before the tab
+  var tabsAfterIndex = this.tabs.splice(index + 1); // Get the tabs after the tab
+
+  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
+
+  // Only change code above this line
+
+  return this;
+ };
+
+// Let's create three browser windows
+var workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+var socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+var videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+
+// Now perform the tab opening, closing, and other operations
+var finalTabs = socialWindow
+  .tabOpen() // Open a new tab for cat memes
+  .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+  .join(workWindow.tabClose(1).tabOpen());
+console.log(finalTabs.tabs);
+
 
 // Functional Programming = Q2/26 - Understanding Functional Programming Terminology
 // TASK: Prepare 27 cups of green tea and 13 cups of black tea and store them in tea4GreenTeamFCC and tea4BlackTeamFCC variables, respectively.
-// Note that the getTea function has been modified so it now takes a function as the first argument.
+// Note We can modify getTea to accept a function as a parameter to be able to change the type of tea it prepares.
+// This makes getTea more flexible, and gives the programmer more control when client requests change.
 // Note: The data (the number of cups of tea) is supplied as the last argument.
 
 // Function that returns a string representing a cup of green tea
@@ -81,6 +127,45 @@ const tea4BlackTeamFCC = null;
 console.log(
   tea4GreenTeamFCC,
   tea4BlackTeamFCC
+);
+
+// MY MODIFIED SOLUTION:
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+// Function that returns a string representing a cup of hibiscus tea
+const prepareHibiscusTea = () => 'hibiscusTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+// // The Modified getTea function to accept a function as a parameter to be able to change the type of tea it prepares.
+
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+const tea4HibiscusTeamFCC = getTea(prepareHibiscusTea, 33);
+// Only change code above this line
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC,
+  tea4HibiscusTeamFCC
 );
 
 
