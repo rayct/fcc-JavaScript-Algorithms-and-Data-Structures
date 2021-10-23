3fc59778a46c94a4c2d4f9966f107ef1f12024f8
rayct <rayct@icloud.com>
2021-10-23 01:29:59 +0100

    CourseWork Update Q=2/21

--- fcc-JavaScript-CourseWork.js
+++ fcc-JavaScript-CourseWork.js
@@ -97,16 +97,17 @@
 // Intermediate Algorithm Scripting = Q2/21 - Diff Two Arrays
 // TASK: 
 // NOTES: Using two for loops and two if statements to find the diff in two arrays
+// Solution 1
 function diffArray(arr1, arr2) {
   var newArr = [];
  
-  for (var i=0; i<arr1.length;i++){
+  for (var i=0; i < arr1.length; i += 1){
      if (arr2.indexOf(arr1[i])===-1){
        newArr.push(arr1[i])
       } 
   
 }
-  for (var x=0; x<arr2.length;x++){
+  for (var x=0; x<arr2.length; x += 1){
     if(arr1.indexOf(arr2[x])===-1){
       newArr.push(arr2[x]);
     }
@@ -115,6 +116,43 @@ function diffArray(arr1, arr2) {
 return newArr; 
 } 
 
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+// Solution 2 <= Refactored - Reusable function
+function diffArray(arr1, arr2) {
+  var newArr = [];
+
+  function comparisonAgainst(first, second) {
+    for (var i = 0; i < first.length; i +=- 1) {
+      if (second.indexOf(first[i]) === -1) {
+        newArr.push(first[i]);
+      }
+    }
+  }
+  
+  comparisonAgainst(arr1, arr2);
+  comparisonAgainst(arr2, arr1);
+
+  return newArr;
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+// Solution 3 <= Refactored with a forEach method
+function diffArray(arr1, arr2) {
+  var newArr = [];
+
+  function comparisonAgainst(first, second) {
+    first.forEach(number => {
+      if (second.indexOf(number) === -1) { newArr.push(number) }
+    });
+  }
+  
+  comparisonAgainst(arr1, arr2);
+  comparisonAgainst(arr2, arr1);
+
+  return newArr;
+}
 console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
 
 
