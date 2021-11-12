baf5da974d5f71d184a36eb05a32b4292f8ef979
rayct <rayct@icloud.com>
2021-11-12 01:38:33 +0000

    CourseWork Update Q=12/21

--- test.js
+++ test.js
@@ -1,13 +1,27 @@
-function fearNotLetter(str) {
-  let alpha = 'abcdefghijklmnopqrstuvwxyz'
-    let startPos = alpha.indexOf(str[0]);
-    let accurateAlpha = alpha.slice(startPos);
-
-    for (let i = 0; i < str.length; i += 1) {
-    if (str[i] != accurateAlpha[i]) {
-      return accurateAlpha[i];
-    }
+// function convertHTML(str) {
+function convertChar(letter) {
+  if (letter === "&") {
+    return "&amp;";
+  } else if (letter === "<") {
+    return "&lt;";
+  } else if (letter === ">") {
+    return "&gt;";
+  } else if (letter === '"') {
+    return "&quot;";
+  } else if (letter === "'") {
+    return "&apos;";
   }
 }
 
-console.log(fearNotLetter("abce"));
+function convertHTML(str) {
+  let charactersToCheckFor = ['&', '<', '>', '"', "'"];
+  for (let i = 0; i < str.length; i += 1) {
+    if (charactersToCheckFor.indexOf(str[i]) != -1) {
+      str = str.slice(0, i) + convertChar(str[i]) + str.slice(i + 1);
+    };
+  }
+  return str;
+
+}
+let result = convertHTML("Dolce & Gabbana");
+console.log(result);
